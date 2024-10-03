ff773266f1ebcc1020eed10273e1d82257fffffc
Vijitha-3 <vijitharajaraman@gmail.com>
2024-10-03 15:31:56 +0530

    Create  Largest product in a series.c

---
+++ Largest product in a series.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int t; 
+    scanf("%d",&t);
+    for(int a0 = 0; a0 < t; a0++){
+        int n; 
+        int k; 
+        scanf("%d %d",&n,&k);
+        char* num = (char *)malloc(512000 * sizeof(char));
+        scanf("%s",num);
+    }
+    return 0;
+}
